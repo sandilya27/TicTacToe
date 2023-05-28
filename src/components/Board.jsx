4f676832b0549c7d9a7c91ae3dc69451731e0826
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2023-05-28 16:51:21 +0530

    Added the functionality to show both X and O

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,18 +3,24 @@ import Square from './Square';
 
 const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClick = clickedPosition => {
+    if(square[clickedPosition]){
+        return;
+    }
     setSquare(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : 'O';
         }
         return squareValue;
       });
     });
+
+    setIsXNext((currentIsXNext) => !currentIsXNext);
   };
-  
+
   const renderSquare = position => {
     return (
       <Square
